ccfe8a901405a4b25eeccfabb488ab0d67e9ad54
hectorLop <lopez.almazan.hector@gmail.com>
2023-04-22 13:03:29 +0200

    Added validity check for having {} at beginning and end of JSON

---
+++ src/check_validity.rs
@@ -0,0 +1,29 @@
+pub fn is_json_valid(content: &str) -> bool {
+    let characters: Vec<char> = content.chars().collect();
+
+    if !(characters[0] == '{' && characters.last() == Some(&'}')) {
+        return false;
+    };
+
+    true
+}
+
+#[cfg(test)]
+mod test {
+    use crate::check_validity::is_json_valid;
+
+    #[test]
+    fn test_invalid_start_and_end_chars() {
+        let no_curly_braces = "\"field\": 2".to_string();
+        assert_eq!(is_json_valid(&no_curly_braces), false);
+
+        let curly_brace_at_beginning = "{\"field\": 2".to_string();
+        assert_eq!(is_json_valid(&curly_brace_at_beginning), false);
+
+        let curly_brace_at_end = "\"field\": 2}".to_string();
+        assert_eq!(is_json_valid(&curly_brace_at_end), false);
+
+        let valid_content = "{\"field\": 2}".to_string();
+        assert_eq!(is_json_valid(&valid_content), true);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::env;
+use std::{env, fs};
 
 mod args;
+mod check_validity;
 
 fn main() {
     let filename: String = match args::validate_args(&env::args().collect()) {
@@ -8,4 +9,11 @@ fn main() {
         Err(err) => panic!("{}", err),
     };
     println!("Filename {}", filename);
+
+    let content = fs::read_to_string(filename).expect("Failed to read the JSON file");
+
+    match check_validity::is_json_valid(&content) {
+        true => (),
+        false => panic!("Invalid JSON"),
+    }
 }
